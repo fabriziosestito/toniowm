a2074d9570b0bbea7dd6503f3d5948f2ca1ba999
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-07-14 08:56:02 +0200

    test: add Vector2D tests

--- src/vector.rs
+++ src/vector.rs
@@ -13,15 +13,6 @@ impl Vector2D {
     }
 }
 
-impl Vector2D {
-    pub fn max(&self, other: Self) -> Self {
-        Self {
-            x: self.x.max(other.x),
-            y: self.y.max(other.y),
-        }
-    }
-}
-
 impl std::ops::Add for Vector2D {
     type Output = Self;
 
@@ -41,5 +32,43 @@ impl ops::Sub for Vector2D {
             x: self.x - other.x,
             y: self.y - other.y,
         }
+    }
+}
+
+impl Vector2D {
+    pub fn max(&self, other: Self) -> Self {
+        Self {
+            x: self.x.max(other.x),
+            y: self.y.max(other.y),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vector2d_add() {
+        let v1 = Vector2D::new(1, 2);
+        let v2 = Vector2D::new(3, 4);
+        let v3 = v1 + v2;
+        assert_eq!(v3, Vector2D::new(4, 6));
+    }
+
+    #[test]
+    fn test_vector2d_sub() {
+        let v1 = Vector2D::new(1, 2);
+        let v2 = Vector2D::new(3, 4);
+        let v3 = v1 - v2;
+        assert_eq!(v3, Vector2D::new(-2, -2));
+    }
+
+    #[test]
+    fn test_vector2d_max() {
+        let v1 = Vector2D::new(1, 2);
+        let v2 = Vector2D::new(3, 4);
+        let v3 = v1.max(v2);
+        assert_eq!(v3, Vector2D::new(3, 4));
     }
 }
